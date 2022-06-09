7fe1951e585d80714966076c8b46ed83c0f2622b
Owais Ibrahim <owaisibrahim@OwaisMacBookPro.local>
2022-06-09 09:45:29 -0500

    adding an about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,30 @@
+import "./About.css";
+import owais from "../assets/profile.jpg";
 import React, { Component } from 'react';
   
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <p>Design your About me page </p> 
+    <div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src = {owais}
+          alt="Profile Pic"
+          ></img>
       </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Owais Ibrahim</div>
+        <div className="brief_description">
+        <p>Hi, my name is Owais Ibrahim! I am a sophmore studying Computer Science at Princeton Univestiy. I am from Chicago, IL and my favorite 
+          food is deep dish Chicago style pizza. </p> 
+        </div>
+      </div>
+    </div>
+  </div>
     )
   }
 }
